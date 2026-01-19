dbc882b3fec815e05a89d0a25c05adb747704e88
Michjzuman <143267862+Michjzuman@users.noreply.github.com>
2026-01-19 23:35:25 +0100

    notations

--- chess.py
+++ chess.py
@@ -1,7 +1,16 @@
 import random
 import os
 
+settings = {
+    "show_piece_symbols": False
+}
+
 def render(board, game):
+    def move_notation(line):
+        move_notation_1 = f"{' ' * 3} {'' if len(game) <= line else game[line]}"
+        move_notation_2 = f"{'' if len(game) <= (line + 1) else (f' {GRAY}-{WHITE} ' + game[line + 1])}"
+        return move_notation_1 + move_notation_2
+
     RESET = "\033[0m"
     WHITE = "\033[37m"
     RESET_TO_WHITE = f"{RESET}{WHITE}"
@@ -10,6 +19,8 @@ def render(board, game):
     YELLOW = "\033[33m"
     BLUE = "\033[34m"
     GRAY = "\033[90m"
+    
+    SYMBOLS = {"K": "♚", "Q": "♛", "R": "♜", "B": "♝", "N": "♞", "P": "♟", " ": " "}
 
     os.system("clear")
     os.system("clear")
@@ -28,15 +39,19 @@ def render(board, game):
             bg_color = '100' if square_white else '40'
             only_bg_color = f"\033[{bg_color};37m"
             color = f"\033[{bg_color}{';37' if piece_white else ';33'}m"
-            render_lines[0] += f"{color} {square.upper()}{RESET_TO_WHITE}{only_bg_color} {'║' if x == len(line) - 1 else'│'}{RESET_TO_WHITE}"
+            piece = SYMBOLS[square.upper()] if settings["show_piece_symbols"] else square.upper()
+            render_lines[0] += f"{color} {piece}{RESET_TO_WHITE}{only_bg_color} {'║' if x == len(line) - 1 else'│'}{RESET_TO_WHITE}"
             render_lines[1] += f"{only_bg_color}{('═' if y == len(board) - 1 else'─') * 3}{('╝' if x == len(line) - 1 else '╧') if y == len(board) - 1 else ('╢' if x == len(line) - 1 else'┼')}{RESET_TO_WHITE}"
-        move_notation_1 = f"{' ' * 3} {'' if len(game) <= moves_line else game[moves_line]}"
-        move_notation_2 = f"{'' if len(game) <= (moves_line + 1) else (f' {GRAY}-{WHITE} ' + game[moves_line + 1])}"
-        render_lines[0] += move_notation_1 + move_notation_2
+        render_lines[0] += move_notation(moves_line)
         for render_line in render_lines:
             print(render_line)
         moves_line += 2
-    print(f"{GRAY}    {'   '.join([str(i + 1) for i in range(8)])}{RESET}")
+    print(f"{GRAY}    {'   '.join([str(i + 1) for i in range(8)])}{RESET} ", move_notation(moves_line))
+    moves_line += 2
+    while moves_line <= len(game):
+        print()
+        print(" " * 34, move_notation(moves_line))
+        moves_line += 2
 
 def imagine_board(game):
     def get_start_board():
@@ -64,6 +79,22 @@ def main():
         "Nf3", "Nf6",
         "Nxe5", "Nxe4",
         "Qc6", "f1",
+        
+        "e4", "e5",
+        "Nf3", "Nf6",
+        "Nxe5", "Nxe4",
+        "Qc6", "f1",
+        
+        "e4", "e5",
+        "Nf3", "Nf6",
+        "Nxe5", "Nxe4",
+        "Qc6", "f1",
+        
+        "e4", "e5",
+        "Nf3", "Nf6",
+        "Nxe5", "Nxe4",
+        "Qc6", "f1",
+        
         "Qxg6#"
     ]
     board = imagine_board(game)
