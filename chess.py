421bf4ec388b9778e1120dce9f6a5f9d95c43638
Michjzuman <143267862+Michjzuman@users.noreply.github.com>
2026-01-19 15:01:39 +0000

    jaaa ÜK fertig

--- chess.py
+++ chess.py
@@ -2,20 +2,21 @@ import random
 import os
 
 def render(board):
-    RESET = "\033[0m"
-    RED = "\033[31m"
-    BLUE = "\033[34m"
-    GRAY = "\033[90m"
     os.system("clear")
     separator = f"+{'+'.join(["---"] * 8)}+\n"
     line = f"|{'|'.join(["   "] * 8)}|\n"
     print(line.join([separator] * 8))
+    
+    for y, line in enumerate(board):
+        print("".join([
+            f"\033[{100 if x % 2 == (0 if y % 2 == 0 else 1) else 40}m {square} \033[0m"
+        for x, square in enumerate(line)]))
 
 def imagine_board(game):
     def get_start_board():
         strong_row = "RNBQKBNR"
         pawn_row = "P" * 8
-        empty_row = [""] * 8
+        empty_row = [" "] * 8
         return [
             list(strong_row),
             list(pawn_row),
